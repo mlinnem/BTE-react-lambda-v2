9d094e83d8d341f7592eaddef67e10eea3cd86f1
Micah Linnemeier <mlinnemus.ibm.com@Micahs-MacBook-Pro.local>
2018-12-10 14:14:45 -0600

    More css refinement

--- bte-frontend/src/AnimalContender.js
+++ bte-frontend/src/AnimalContender.js
@@ -23,7 +23,7 @@ render () {
     <Box className={this.props.animationState + " animal box"} pose={this.props.animationState} onClick={this.props.onBallotClick}>
     <img className="photo" src="./rhino.png"/>
     <span className="animalName">{this.props.animal.Name}</span>
-    </Box >
+    </Box>
   </span>
 </span>
 )
@@ -36,12 +36,13 @@ animalContender_mapDispatchToProps
 
 
 const Box = posed.div({
-  hiddenleft: { x: '-100vw' , transition: {x : {ease: 'easeOut', duration: 1350}}},
-  incomingleft: { x: '0vw' , transition: {x : {ease: 'easeOut', duration: 1350}}},
+  hiddenleft: { x: '-100vw' , transition: {x : {ease: 'easeOut', duration: 0}}},
   outgoingleft: {y: '100vh', transition: {y : {ease: 'easeOut', duration: 550}}},
-  hiddenright: { x: '100vw', transition: {x : {ease: 'easeOut', duration: 1350}}},
+   outgoingright: {x: '0vw', y:'100vh',  transition: {y : {ease: 'easeOut', duration: 550}}},
+  incomingleft: { x: '0vw' , transition: {x : {ease: 'easeOut', duration: 1350}}},
+  hiddenright: { x: '100vw', transition: {x : {ease: 'easeOut', duration: 0}}},
   incomingright: { x: '0vw', transition: {x : {ease: 'easeOut', duration: 1350}}},
-  outgoingright: {x: '0vw', y:'100vh',  transition: {y : {ease: 'easeOut', duration: 550}}}
+
 });
 
 export default AnimalContender;
--- bte-frontend/src/Ballot.js
+++ bte-frontend/src/Ballot.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import AnimalContender from "./AnimalContender"
-
+import posed from 'react-pose';
 const ReactRedux = require('react-redux');
 
 const ballot_mapStateToProps= (state, ownProps) => { return {
 ballot: state.ballots.ballotStore[ownProps.id],
 }};
+
+
 class Ballot_View extends React.Component {
 render () {
   console.log("re-rendering ballot");
@@ -16,7 +18,7 @@ render () {
   }
   console.log(this.props.ballot.QueueState);
   return (
-  <div className={"ballot " + this.props.ballot.QueueState + "ballot"}>
+  <div className={"ballot fred " + this.props.ballot.QueueState + "ballot"} pose={this.props.ballot.QueueState}>
     <AnimalContender id={this.props.ballot.Animal1ID} animationState={this.props.ballot.QueueState + "left"} side={SIDE.LEFT}/>
     <AnimalContender id={this.props.ballot.Animal2ID} animationState={this.props.ballot.QueueState + "right"} side={SIDE.RIGHT}/>
   </div>
