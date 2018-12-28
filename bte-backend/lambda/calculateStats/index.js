9a1b90e5ac5f571b77d73d7ca815cd6ead695439
mlinnem <micah.linnemeier@gmail.com>
2018-12-28 14:00:45 -0600

    Refactored to have animalCount stat and also for createBallots to use this stat

--- bte-backend/lambda/calculateStats/index.js
+++ bte-backend/lambda/calculateStats/index.js
@@ -9,21 +9,24 @@ exports.handler =  (event) => {
 };
 
 function calculateStats() {
-  console.log("Calculate rankings...");
-  calculateRankings();
+  console.log("Calculate animal statistics...");
+  calculateAnimalStatistics();
 }
 
-async function calculateRankings() {
-  console.log("    Get animals");
+async function calculateAnimalStatistics() {
+  console.log("Get animals");
   var animals = await backend_getAnimals();
+  console.log(animals);
 
+  console.log("Calculating rankings...");
   var animalIDAndWinPercentages = [];
   console.log("    For each animal...");
-  var animalIDs = Object.keys(animals);
-  for (let animalID in animalIDs) {
-    var animal = animalIDs[animalID];
+  for (let animalID in animals) {
+    var animal = animals[animalID];
+    console.log(animal);
     console.log("        Calculate win %");
     var winPercentage = animal.Wins / (animal.Wins + animal.Losses);
+    console.log(winPercentage);
     var animalIDAndWinPercentage = {
       'ID' : animal.ID,
       'WinPercentage': winPercentage,
@@ -31,23 +34,32 @@ async function calculateRankings() {
     animalIDAndWinPercentages.push(animalIDAndWinPercentage);
   }
 
-  //TODO: Double check order, highest first or lowest first?
+
+  console.log(animalIDAndWinPercentages);
   console.log("    Sort animals by win %");
-  animalIDAndWinPercentages.sort((a,b) => {return a.WinPercentage - b.LossPercentage});
+  animalIDAndWinPercentages.sort((a,b) => {return b.WinPercentage - a.WinPercentage});
+  console.log(animalIDAndWinPercentages);
   var animalIDsByWinPercentage = animalIDAndWinPercentages.map((animal) => {return animal.ID});
+  console.log(animalIDsByWinPercentage);
 
   console.log("    Prepare database update");
   var putRankingsStatement = makePutRankingsStatement(animalIDsByWinPercentage);
 
+  console.log(" Calculate animalCount");
+  var animalCount = animals.length;
+
+  console.log("    Prepare database update")
+  var putAnimalCountStatement = makePutAnimalCountStatement(animalCount);
+
   console.log("Write all updates to backend");
-  backend_writeAllUpdatesToBackend(putRankingsStatement);
+  backend_writeAllUpdatesToBackend(putRankingsStatement, putAnimalCountStatement);
 }
 
 function makePutRankingsStatement(animalsSortedByWinPercentage) {
   var put_params =
   { "Item": {
     "Statistic" : "Rankings",
-   "Rankings": animalsSortedByWinPercentage,
+   "Value": animalsSortedByWinPercentage,
   },
   "TableName" : "SummaryStatistics"
  };
@@ -57,10 +69,24 @@ function makePutRankingsStatement(animalsSortedByWinPercentage) {
  return put_params;
 }
 
+function makePutAnimalCountStatement(animalCount) {
+  var put_params =
+  { "Item": {
+    "Statistic" : "AnimalCount",
+   "Value": animalCount,
+  },
+  "TableName" : "SummaryStatistics"
+ };
+
+ console.log("...putAnimalCount params:");
+ console.log(put_params);
+ return put_params;
+}
+
 //--BACKEND--
 
 function backend_getAnimals() {
-  var scan_params = {
+  var scan_params = { //TODO: Change to scan perhaps if performance is an issue?
     TableName : 'Animals',
   };
 
@@ -70,7 +96,7 @@ function backend_getAnimals() {
   var promise = io.scan(scan_params).promise().then((result) => {
     console.log("...get_animals result:");
     console.log(result);
-    return result;
+    return result.Items;
   }).catch((error) => {
     console.error(error);
     return error;
@@ -79,15 +105,16 @@ function backend_getAnimals() {
   return promise;
 }
 
-function backend_writeAllUpdatesToBackend(putRankingsStatement) {
-  var promise = io.put(putRankingsStatement).promise().then((result) => {
-    console.log("...put_rankings_statement result:");
-    console.log(result);
-    return result.Items;
+function backend_writeAllUpdatesToBackend(putRankingsStatement, putAnimalCountStatement) {
+  var putRankingsPromise = io.put(putRankingsStatement).promise();
+  var putAnimalCountStatement = io.put(putAnimalCountStatement).promise();
+  return Promise.all([putRankingsPromise, putAnimalCountStatement])
+  .then((results) => {
+    console.log("...put_statistics results:");
+    console.log(results);
+    return results
   }).catch((error) => {
     console.error(error);
     return error;
   });
-
-  return promise;
 }
--- bte-backend/lambda/getAnimals/index.js
+++ bte-backend/lambda/getAnimals/index.js
@@ -3,16 +3,24 @@ AWS.config.update({region: 'us-east-1'});
 
 var io = new AWS.DynamoDB.DocumentClient({apiVersion: '2018-10-01'});
 
-exports.handler =  (event) => {
-    console.log("Event up in here.");
-    console.log(event);
-    var response = getAnimals()
-    .then((animalsResponse) => {
-      console.log("ARRRRRR");
-      console.log(animalsResponse);
-      var animals = animalsResponse.Item.Animals;
-      var animalIDsInRankOrder = getAnimalIDsInRankOrder(animals);
-      const response = {
+ exports.handler =  (event) => {
+   return getAnimals();
+};
+
+async function getAnimals() {
+    var getAnimalsPromise = backend_getAnimals();
+    var getRankingsPromise = backend_getRankings();
+
+    console.log("Get Animals and Animal Rankings");
+    var animals, rankings;
+    [animals, rankings] = await Promise.all([getAnimalsPromise,getRankingsPromise]);
+
+    console.log("Produce response based on results");
+    return generateResponse(animals, rankings);
+}
+
+function generateResponse(animals, rankings) {
+    const response = {
         statusCode: 200,
         headers: {
           "Access-Control-Allow-Origin" : "*",
@@ -20,67 +28,58 @@ exports.handler =  (event) => {
           "Access-Control-Allow-Headers" : "*"},
         body: JSON.stringify({
           "AnimalStore" : animals,
-          "AnimalIDsInRankOrder" : animalIDsInRankOrder
+          "AnimalIDsInRankOrder" : rankings
         })
       };
-    console.log("response:");
-    console.log(response);
-    return response;
-    });
-    return response;
-};
-
-function getAnimals() {
-  var get_params = {
-  Key: {
-   "ID": "0"
-  },
-  TableName: "AllAnimals"
- };
-
- return io.get(get_params).promise();
+      console.log(response);
+      return response;
 }
 
-function getAnimalIDsInRankOrder(animalsMap) {
-  console.log("RANK ANIMALS");
-  console.log(animalsMap);
-    var animalKeys = Object.keys(animalsMap);
-    var animals = [];
-    for (let animalKey of animalKeys) {
-      console.log("X");
-      var animal = animalsMap[animalKey];
-      animal.ID = animalKey;
-      animals.push(animal);
+
+//--BACKEND--
+
+function backend_getAnimals() {
+  var scan_params = { //TODO: Handle case where payload is > 1 MB (pagination?)
+    TableName : 'Animals',
+  };
+
+  console.log("...get_animals_params:");
+  console.log(scan_params);
+
+  var promise = io.scan(scan_params).promise().then((result) => {
+    console.log("...get_animals result:");
+    console.log(result);
+
+    var animalsArray = result.Items;
+
+    var animalsByID = {};
+    for (let animalIndex in animalsArray) {
+      var animal = animalsArray[animalIndex];
+      animalsByID[animal.ID] = animal;
     }
-      animals.sort(function (a, b) {
-      var ratio_animal_a = (a.Wins + 1) / (a.Losses + 1);
-      var ratio_animal_b = (b.Wins + 1) / (b.Losses + 1);
-      console.log("ratio_animal_a:");
-      console.log(ratio_animal_a);
-      console.log("ratio_animal_b:");
-      console.log(ratio_animal_b);
-      if (ratio_animal_a == ratio_animal_b) {
-        var alphabeticalOrder = [a.Name, b.Name].sort();
-        if (alphabeticalOrder[0] == a.Name) {
-          console.log("-1");
-          return -1;
-        } else {
-          console.log("1");
-          return 1;
-        }
-      } else {
-        if (ratio_animal_b > ratio_animal_a) {
-          console.log("1");
-          return 1;
-        } else if (ratio_animal_b < ratio_animal_a) {
-          console.log("-1");
-          return -1;
-        } else {
-          console.log("0");
-          return 0;
-        }
-      }
-    });
-    var animalIDsByRank = animals.map((animal) => {return animal.ID});
-    return animalIDsByRank;
+
+    return animalsByID;
+  }).catch((error) => {
+    console.error(error);
+    return error;
+  });
+
+  return promise;
+}
+
+function backend_getRankings() {
+  var get_params = {
+  "Key": {
+   "Statistic": "Rankings",
+  },
+  TableName: "SummaryStatistics"
+ };
+
+ return io.get(get_params).promise().then((result) => {
+    console.log("get rankings result:");
+   console.log(result);
+   return result.Item.Value;
+ }).catch((error) => {
+   return error;
+ });
 }
