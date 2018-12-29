8b8f8f24707a440048fe0171a16bd0cbd6af6d3e
mlinnem <micah.linnemeier@gmail.com>
2018-12-28 20:14:19 -0600

    w00t w00t shared variables and maybe code between server and client, all files this time

--- bte-frontend/src/Header.js
+++ bte-frontend/src/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+const Constants = require("./c_constants");
+
 class Header extends React.Component {
 render () {
-  return <div className="header"><span className="header-text">Both Are Totally Enraged</span></div>;
-}
+  return <div className="header"><span className="header-text">{Constants.SESSION_IDENTIFIER}</span></div>;
+  }
 }
 
 export default Header;
