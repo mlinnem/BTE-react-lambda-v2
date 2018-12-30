7e1371ebdd8df072947f5e5ff7034a5dc6afa083
mlinnem <micah.linnemeier@gmail.com>
2018-12-29 18:03:18 -0600

    Basic matchup stats being displayed, other stuff seems jacked up though. Investigating

--- bte-frontend/src/Header.js
+++ bte-frontend/src/Header.js
@@ -4,7 +4,7 @@ const Constants = require("./c_constants");
 
 class Header extends React.Component {
 render () {
-  return <div className="header"><span className="header-text">{Constants.SESSION_IDENTIFIER}</span></div>;
+  return <div className="header"><span className="header-text">Both Are Totally Enraged</span></div>;
   }
 }
 
--- bte-frontend/src/ResultsPanel.js
+++ bte-frontend/src/ResultsPanel.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import './index.scss';
 import * as ar from "./actionsAndReducers";
+const u = require("./c_utilityFunctions");
 const ReactRedux = require('react-redux');
 
 const resultsPanel_mapStateToProps = (state, ownProps) => {return {
 animalStore : state.animals.animalStore,
-rankOrder: state.animals.rankOrder,
+ballotStore: state.ballots.ballotStore,
+outgoingBallotID: state.ballots.outgoingBallotID,
 }};
 
 class ResultsPanel_View extends React.Component {
 render() {
-    return <div className={"resultsPanel"}></div>;
+    if (this.props.outgoingBallotID) {
+      var outgoingBallot = this.props.ballotStore[this.props.outgoingBallotID];
+      var animal1 = this.props.animalStore[outgoingBallot.Animal1ID];
+      var animal2 = this.props.animalStore[outgoingBallot.Animal2ID];
+      u.t("animal1:", 1);
+      u.t_o(animal1);
+      u.t("animal2:", 1);
+      u.t_o(animal2);
+
+      var animal1Wins = outgoingBallot.MatchupData.Animal1Wins;
+      var animal2Wins = outgoingBallot.MatchupData.Animal1Wins; //TODO: Make sure these aren't flipped, on server
+      return <div className={"resultsPanel"}>{animal1.Name} with {animal1Wins}, {animal2.Name} with {animal2Wins}</div>;
+    } else {
+      return <div className={"resultsPanel"}></div>;
+    }
+
   }
 }
 
